d8e4d55e5d40050edfff509390addabba4990475
Mark Scherschel II <schersch@gmail.com>
2016-01-16 16:05:44 -0500

    added views

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -4,6 +4,9 @@
 -- Get rid of existing tables that might cause errors
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Players;
+DROP VIEW IF EXISTS MatchCounts;
+DROP VIEW IF EXISTS WinCounts;
+DROP VIEW IF EXISTS Standings;
 
 -- Players Table
 -- Holds each player's name
@@ -19,3 +22,26 @@ CREATE TABLE Matches (
     winner INT REFERENCES Players(id),
     loser INT REFERENCES Players(id)
 );
+
+-- WinCounts View
+-- Shows the current Win Count for each player
+CREATE VIEW WinCounts AS
+    SELECT Players.id, COUNT(Matches.winner) AS total
+    FROM Players JOIN Matches
+    ON Players.id = Matches.winner
+    GROUP BY Players.id;
+
+-- MatchCounts View
+-- Shows the current Match Count for each player
+CREATE VIEW MatchCounts AS
+    SELECT Players.id, Count(Matches.id) AS total
+    FROM Players JOIN Matches
+    ON Players.id = Matches.winner OR Players.id = Matches.loser
+    GROUP By Players.id;
+    
+-- Standings View
+-- Shows the number of wins and matches for each player
+CREATE VIEW Standings AS
+    Select Players.id, Players.name, WinCounts.total as wins, MatchCounts.total as matches
+    FROM Players, MatchCounts, WinCounts
+    WHERE Players.id = WinCounts.id AND WinCounts.id = MatchCounts.id;
