21cf274d4dc4d4e89d240b1cdd5c6e46c63c7d22
Mark Scherschel II <schersch@gmail.com>
2016-01-16 14:18:37 -0500

    created table definitions

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,9 +1,21 @@
 -- Table definitions for the tournament project.
 --
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+-- Get rid of existing tables that might cause errors
+DROP TABLE IF EXISTS Matches;
+DROP TABLE IF EXISTS Players;
 
+-- Players Table
+-- Holds each player's name
+CREATE TABLE Players (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+-- Matches Table
+-- Holds the result of a match between two players
+CREATE TABLE Matches (
+    id SERIAL PRIMARY KEY,
+    winner INT REFERENCES Players(id),
+    loser INT REFERENCES Players(id)
+);
